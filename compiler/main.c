f22553e4c2808bd6d58e5b560840a9692b70b2dd
Bob van Dam <bvandam@telis.nl>
2023-03-04 17:01:02 +0100

    finished boilerplate to compile test.c

--- compiler/main.c
+++ compiler/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-
+#include "helpers/vector.h"
+#include "compiler.h"
 
 int main(int argc, char* argv[])
 {
-    printf("Hello world!\n");
+    int res = compile_file("./test.c", "./test", 0);
+
+    if (COMPILER_FILE_COMPILED_OK == res)
+    {
+        printf("Compiled OK\n");
+    } 
+    else if (COMPILER_FAILED_WITH_ERRORS == res)
+    {
+        printf("Compilation failed\n");
+    } 
+    else 
+    {
+        printf("Unknown response from compile file\n");
+    }
+
     return 0;
 }
